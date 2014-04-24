33bbbdd5e7e9dcc639949127e3c2e4ecd1b92fd9
Glowx13 <glowx13@gmail.com>
2014-04-24 14:06:41 +1000

    Create Maze.java -> Simplest Maze Interface
    Maze Interface

---
+++ Maze.java
@@ -0,0 +1,59 @@
+/**
+ * Maze Interface
+ *
+ * @author AwolGerbil
+ */
+
+public class Maze {
+    /**
+     * Generates a Maze
+     *
+     * @param height
+     *            The height of the Maze
+     * @param width
+     *            The width of the Maze
+     * 
+     * @return The resulting Maze
+     */
+    public Maze generateMaze(int height, int width);
+    
+    /**
+     * Returns whether there is a wall above given Coordinate
+     * 
+     * @param c
+     *            The Coordinate
+     * 
+     * @return whether there is a wall above the Coordinate
+     */
+    public boolean wallUp(Coordinate c);
+    
+    /**
+     * Returns whether there is a wall to the right of given Coordinate
+     * 
+     * @param c
+     *            The Coordinate
+     * 
+     * @return whether there is a wall to the right of the Coordinate
+     */
+    public boolean wallRight(Coordinate c);
+    
+    /**
+     * Returns whether there is a wall below given Coordinate
+     * 
+     * @param c
+     *            The Coordinate
+     * 
+     * @return whether there is a wall below the Coordinate
+     */
+    public boolean wallDown(Coordinate c);
+    
+    /**
+     * Returns whether there is a wall to the left of given Coordinate
+     * 
+     * @param c
+     *            The Coordinate
+     * 
+     * @return whether there is a wall to the left of the Coordinate
+     */
+    public boolean wallLeft(Coordinate c);
+}
